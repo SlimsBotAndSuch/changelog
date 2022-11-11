7db25aef4bcbf82a344e77ef29538e35f899bbb5
Slim <72360637+SlimsBotAndSuch@users.noreply.github.com>
2022-11-11 11:20:04 -0700

    Update and rename Official Changelog For SMB to Official Changelog For SMB.py

---
+++ Official Changelog For SMB.py
@@ -0,0 +1,26 @@
+8/21/22 "5:57am EST" Changed /members to /serverstats.
+8/21/22 "6:59am EST" Added /say command.
+8/25/22 "5:53am EST" Added new commands to /cmds.
+8/27/22 Changed From regular python to pycord.
+8/28/22 Inputted application commands.
+9/2/22 Fixed command timestamps, Added calculator commands.
+9/3/22 Fixed application commands showing As errors.
+9/9/22 Added multiply command.
+9/9/22 "5:15pm EST" Added a coinflip command.
+9/9/22 "5:54pm EST" Added _8ball command.
+9/10/22 Bot now has a status.
+9/10/22 "5:01pm EST" Added suggest command.
+9/15/22 "4:58pm EST" Added buttons to _help command.
+9/15/22 "9:50pm EST" Added dicerole And creatembed command.
+9/23/22 Added simpdetector command, added to whois command.
+9/24/22 Added cooldown to suggest, added botinfo command.
+10/24/22 Added pms command, tuneups.
+11/7/22 Added viewable messagecount, made moderation commands give more Info.
+11/8/22 Adding faulty bump reminder.
+11/9/22 Finished adding fixes to mod commands.
+11/10/22 Finished making leaderboard command neat.
+11/10/22 "8:03pm EST" Made suggestion allowed _for owner use.
+11/10/22 "8:50pm EST" Replaced code With botinfo.
+11/10/22 "11:07pm EST" Added Total Deleted Messages leaderboard.
+11/11/22 "9:15am EST" Added runtime command.
+11/11/22 "10:28am EST" Made Help command more neat.
